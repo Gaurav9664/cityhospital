22d7032d2a50651352bed64780fc5ba5e39951f4
Gaurav9664 <gauravgaming1206@gmail.com>
2023-06-27 22:53:32 +0530

    27_06_adminpage

--- src/App.js
+++ src/App.js
@@ -1,46 +1,61 @@
-import Header from './component/Header';
-import Footer from './component/Footer';
-import Home from './container/Home';
-import Departments from './container/Departments';
-import Doctors from './container/Doctors';
-import About from './container/About';
-import Contact from './container/Contact';
+import Header from './user/component/Header';
+import Footer from './user/component/Footer';
+import Home from './user/container/Home';
+import Departments from './user/container/Departments';
+import Doctors from './user/container/Doctors';
+import About from './user/container/About';
+import Contact from './user/container/Contact';
 import { Routes, Route } from 'react-router-dom'
-import Doctor from './container/Doctor';
-import VisitingDoctor from './container/VisitingDoctor';
-import Appointment from './container/Appointment';
-import NotFound from './container/NotFound'
-import Auth from './container/Auth'
+import Doctor from './user/container/Doctor';
+import VisitingDoctor from './user/container/VisitingDoctor';
+import Appointment from './user/container/Appointment';
+import NotFound from './user/container/NotFound'
+import Auth from './user/container/Auth'
 import Fromvalidetion from './test/Fromvalidetion';
-import Auth1 from './container/Auth1';
+import Auth1 from './user/container/Auth1';
+import Muidrawer from './admin/component/Muidrawer';
+import Medicine from './admin/container/Medicine';
+import Product from './admin/container/Product';
+import AdminDoctor from './admin/container/AdminDoctor';
+import Adminppointment from './admin/container/Adminppointment';
 
 function App() {
   return (
     <>
-      <Header />
+      <Muidrawer />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/departments' element={<Departments />} />
-        <Route path='/doctors' element={<Doctors />} />
-        <Route path='/doctors/:id' element={<Doctor />} />
-        <Route path='/doctors/Visiting_Doctor' element={<VisitingDoctor />} />
-
-        <Route path='/doctors'>
-          <Route path='/doctors/:id' element={<Doctor />} />
-          <Route path='/doctors/Visiting_Doctor' element={<VisitingDoctor />} />
-        </Route>
-
-
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/appointment' element={<Appointment />} />
-        <Route path='*' element={<NotFound />} />
-        {/* <Route path='/auth' element={<Auth />} /> */}
-        <Route path='/auth' element={<Auth1 />} />
-        <Route path='/Fromvalidetion' element={<Fromvalidetion />} />
+          <Route path='/Medicine' element={<Medicine />} />
+          <Route path='/Product' element={<Product />} />
+          <Route path='/Doctor' element={<AdminDoctor />} />
+          <Route path='/Appointment' element={<Adminppointment />} />
       </Routes>
-      <Footer />
     </>
+    // <>
+    //   <Header />
+    //   <Routes>
+    //     <Route path='/' element={<Home />} />
+    //     <Route path='/departments' element={<Departments />} />
+    //     <Route path='/doctors' element={<Doctors />} />
+    //     <Route path='/doctors/:id' element={<Doctor />} />
+    //     <Route path='/doctors/Visiting_Doctor' element={<VisitingDoctor />} />
+
+    //     <Route path='/doctors'>
+    //       <Route path='/doctors/:id' element={<Doctor />} />
+    //       <Route path='/doctors/Visiting_Doctor' element={<VisitingDoctor />} />
+    //     </Route>
+
+
+    //     <Route path='/about' element={<About />} />
+    //     <Route path='/contact' element={<Contact />} />
+    //     <Route path='/appointment' element={<Appointment />} />
+    //     <Route path='*' element={<NotFound />} />
+    //     {/* <Route path='/auth' element={<Auth />} /> */}
+    //     <Route path='/auth' element={<Auth1 />} />
+    //     <Route path='/Fromvalidetion' element={<Fromvalidetion />} />
+    //   </Routes>
+    //   <Footer />
+    // </>
+    
   );
 }
 
---
+++ src/admin/container/Adminppointment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Adminppointment(props) {
+    return (
+        <div>
+            <h1>Adminppointment</h1>
+        </div>
+    );
+}
+
+export default Adminppointment;
