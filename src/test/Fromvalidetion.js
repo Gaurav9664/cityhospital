6701c9f3bba89c120dabefc2ee1496f2e5f06dbc
patelmonik <patelmonik1028@gmail.com>
2023-06-27 09:17:45 +0530

    27_06_MUI

--- src/test/Fromvalidetion.js
+++ src/test/Fromvalidetion.js
@@ -12,7 +12,7 @@ function Fromvalidetion(props) {
             .test("Fullname", "Enter valid name", function (value) {
                 let arr = value.split(" ");
 
-                if (arr.length >= 3) {
+                if (arr.length === 3) {
                     return true
                 }else {
                     return false
@@ -26,12 +26,19 @@ function Fromvalidetion(props) {
                 /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
                 "Password must contain at least 8 characters, one uppercase, one number and one special case character"
             ),
-        ConfPassword: yup.string().oneOf([yup.ref('Password'), null], 'Passwords must match'),
-        PhoneNumber: yup.number().required("please enter your number").test('PhoneNumber', 'Must be exactly 10 number.', (value) => { if (value) { return value.toString().length === 10 } }),
+        ConfPassword: yup.string().required().test('ConfPassword', 'Not Same', function (val) {
+            if (this.parent.Password === val) {
+                return true
+            }else {
+                return false
+            }
+
+        }),
+        PhoneNumber: yup.number().required("please enter your number"),
         Age: yup.number().required("Please enter your age").min(0, "You must be at least 0 years").max(150, "You must be at most 150 years"),
         Gender: yup.string().required(),
         Country: yup.string().required(),
-        Hobby: yup.array().min(2).of(yup.string().required()).required(),
+        Hobby: yup.array().min(2, 'please Selet 2 Hobby').required(),
         Address: yup.string().required('Please enter your massage').test('Address', 'maxmium 5 word allowed.', function (val) {
             let arr = val.split(" ");
 
@@ -76,6 +83,9 @@ function Fromvalidetion(props) {
 
         <section id="contact" className="contact">
             <div className="container">
+            <div className="section-title">
+                    <h2>Form Validation</h2>
+                </div>
                 <div className="col-lg-8 mt-5 mt-lg-0">
                     <form onSubmit={handleSubmit} className="php-email-form">
                         <div className="row">
@@ -283,7 +293,7 @@ function Fromvalidetion(props) {
                                 <span className='error'>{errors.Accept && touched.Accept ? errors.Accept : ''}</span>
                             </div>
 
-                            <div className="text-center"><button type="submit">Send Message</button></div>
+                            <div className="text-center"><button type="submit">submit</button></div>
                         </div>
                     </form>
                 </div>
